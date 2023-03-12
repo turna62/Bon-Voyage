65f3293c11ca7503ab977cfb41495b50be6a69d2
shaisad <95139737+shaisad@users.noreply.github.com>
2023-03-12 23:03:36 +0600

    updated version of search

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
-import "./searchstyle.css";
-import data from "./places_data.json";
 
+import Search from "./Search";
 
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
@@ -12,47 +11,7 @@ import LogIn from "./user_creation/login";
 import HomePage from "./Home/home";
 import TripPlan from "./Home/PlanATrip";
 
-function Search() {
-  const [searchTerm, setSearchTerm] = useState("");
-  const filteredData = data.filter(
-    (val) =>
-      val.place_name.toLowerCase().includes(searchTerm.toLowerCase()) ||
-      val.place_category.toLowerCase().includes(searchTerm.toLowerCase())
-  );
 
-  return (
-    <>
-    <body class="sbody">
-      <div className="templateContainer">
-        <div className="searchInput_Container">
-          
-          <form class="search" action="">
-                   <input class="searchi" id="searchInput" type="text" placeholder="Search here..." onChange={(event) => {setSearchTerm(event.target.value);}}/>
-                   <button type="submit">Search</button>
-          </form>   
-        </div>
-        {searchTerm === "" ? (
-          <div className="no_results_found"></div>
-        ) : filteredData.length > 0 ? (
-          <div className="template_Container">
-            {filteredData.map((val) => {
-              return (
-                <div className="template" key={val.place_id}>
-                  <img src={process.env.PUBLIC_URL + val.place_image} alt="" />
-                  <h5>{val.place_name}</h5>
-                  <p>{val.place_description}</p>
-                </div>
-              );
-            })}
-          </div>
-        ) : (
-          <div className="no_results_found">No results found.</div>
-        )}
-      </div>
-      </body>
-    </>
-  );
-}
 
 function App() {
   // const isLoggedIn = window.localStorage.getItem("loggedIn");
