09c84950639bee0a7b35c76d836c2c82b901251d
Lamia Alam Turna <95170568+turna62@users.noreply.github.com>
2023-03-12 23:38:46 +0600

    add

--- src/Search.js
+++ src/Search.js
@@ -1,4 +1,5 @@
 import "./searchstyle.css";
+//import "./Home/HomeCss/styles.css";
 import data from "./places_data.json";
 import React, { useState } from "react";
 
@@ -12,12 +13,34 @@ function Search() {
   
     return (
       <>
-      <body class="sbody">
+      <div class="scbody">
+
+      <nav class="navbar navbar-expand-md navbar-dark navbar-custom fixed-top">
+                
+                <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link page-scroll" href="http://localhost:3000">HOME <span class="sr-only">(current)</span></a>
+                        </li>
+        
+                        <li class="nav-item">
+                            <a class="nav-link" href="http://localhost:3000/sign-up">SIGN UP</a>
+                            
+                        </li>
+
+                        
+                  </ul>
+                            
+              </div>
+         </nav> 
+
+            
+        
         <div className="templateContainer">
           <div className="searchInput_Container">
             
             <form class="search" action="">
-                     <input class="searchi" id="searchInput" type="text" placeholder="Search here..." onChange={(event) => {setSearchTerm(event.target.value);}}/>
+                     <input class="searchi" id="searchInput" type="text" placeholder="Search places..." onChange={(event) => {setSearchTerm(event.target.value);}}/>
                      <button type="submit">Search</button>
             </form>   
           </div>
@@ -39,7 +62,7 @@ function Search() {
             <div className="no_results_found">No results found.</div>
           )}
         </div>
-        </body>
+        </div>
       </>
     );
   }
