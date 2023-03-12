5df46d2858f3bbf128b675ca9544c3d1da9dc693
Lamia Alam Turna <95170568+turna62@users.noreply.github.com>
2023-03-13 03:33:24 +0600

    add

---
+++ src/MyTrip/PlanATrip.js
@@ -0,0 +1,196 @@
+import React from 'react';
+ import '../Home/HomeCss/styles.css';
+ import './css/bootstrap.min.css';
+ import './css/datepicker.css';
+ import'./css/tooplate-style.css';
+ 
+ class TripPlan extends React.Component{
+
+    render(){
+
+        return(
+             <div class="planbody">
+    
+    
+	{/* <div class="spinner-wrapper">
+        <div class="spinner">
+            <div class="bounce1"></div>
+            <div class="bounce2"></div>
+            <div class="bounce3"></div>
+        </div>
+    </div> */}
+  
+
+   
+    <nav class="navbar navbar-expand-md navbar-dark navbar-custom fixed-top">
+        
+        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link page-scroll" href="#header">HOME <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link page-scroll" href="#intro">INTRO</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link page-scroll" href="#services">SERVICES</a>
+                </li>
+                
+
+                <li class="nav-item">
+                    <a class="nav-link" href="user_creation\login.html">SIGN UP</a>
+                    
+                </li>
+
+                <li class="nav-item-search">
+                    
+                    <butaton type="button" class="btn text-light ms-3" data-bs-toggle="modal" data-bs-target="#searchModal"><i class="fa fa-search"></i></butaton>
+                    
+                </li>
+            </ul>
+                    </div>
+    </nav>
+   
+    
+    <header id="header" class="header">
+        <div class="header-content">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                    <div class="text-container">
+                                    <h1>Bon <span id="js-rotating">VOYAGE!</span></h1>
+                                    <p class="p-heading p-large">The journey of a thousand miles begins with a single step.</p>
+                                      
+
+                                
+
+    <div class="tm-section tm-bg-img" id="tm-section-1">
+                <div class="tm-bg-white ie-container-width-fix-2">
+                    <div class="container ie-h-align-center-fix">
+                        <div class="row">
+                            <div class="col-xs-12 ml-auto mr-auto ie-container-width-fix">
+                                <form action="index.html" method="get" class="tm-search-form tm-section-pad-2">
+                                    <div class="form-row tm-search-form-row">
+                                        <div class="form-group tm-form-element tm-form-element-100">
+                                            <i class="fa fa-map-marker fa-2x tm-form-element-icon"></i>
+                                            <input name="city" type="text" class="form-control" id="inputCity" placeholder="Type your destination..."/>
+                                        </div>
+                                        <div class="form-group tm-form-element tm-form-element-50">
+                                            <i class="fa fa-calendar fa-2x tm-form-element-icon"></i>
+                                            <input name="check-in" type="text" class="form-control" id="inputCheckIn" placeholder="Check In"/>
+                                        </div>
+                                        <div class="form-group tm-form-element tm-form-element-50">
+                                            <i class="fa fa-calendar fa-2x tm-form-element-icon"></i>
+                                            <input name="check-out" type="text" class="form-control" id="inputCheckOut" placeholder="Check Out"/>
+                                        </div>
+                                    </div>
+                                    <div class="form-row tm-search-form-row">
+                                        <div class="form-group tm-form-element tm-form-element-2">                                            
+                                            <select name="adult" class="form-control tm-select" id="adult">
+                                                <option value="">Adult</option>
+                                                <option value="1">1</option>
+                                                <option value="2">2</option>
+                                                <option value="3">3</option>
+                                                <option value="4">4</option>
+                                                <option value="5">5</option>
+                                                <option value="6">6</option>
+                                                <option value="7">7</option>
+                                                <option value="8">8</option>
+                                                <option value="9">9</option>
+                                                <option value="10">10</option>
+                                            </select>
+                                            <i class="fa fa-2x fa-user tm-form-element-icon"></i>
+                                        </div>
+                                        <div class="form-group tm-form-element tm-form-element-2">                                            
+                                            <select name="children" class="form-control tm-select" id="children">
+                                                <option value="">Children</option>
+                                                <option value="0">0</option>
+                                                <option value="1">1</option>
+                                                <option value="2">2</option>
+                                                <option value="3">3</option>
+                                                <option value="4">4</option>
+                                                <option value="5">5</option>
+                                                <option value="6">6</option>
+                                                <option value="7">7</option>
+                                                <option value="8">8</option>
+                                                <option value="9">9</option>
+                                                <option value="10">10</option>
+                                            </select>
+                                            <i class="fa fa-user tm-form-element-icon tm-form-element-icon-small"></i>
+                                        </div>
+                                        <div class="form-group tm-form-element tm-form-element-2">
+                                            <select name="room" class="form-control tm-select" id="room">
+                                                <option value="">Room</option>
+                                                <option value="1">1</option>
+                                                <option value="2">2</option>
+                                                <option value="3">3</option>
+                                                <option value="4">4</option>
+                                                <option value="5">5</option>
+                                                <option value="6">6</option>
+                                                <option value="7">7</option>
+                                                <option value="8">8</option>
+                                                <option value="9">9</option>
+                                                <option value="10">10</option>
+                                            </select>
+                                            <i class="fa fa-2x fa-bed tm-form-element-icon"></i>
+                                        </div>
+                                        <div class="form-group tm-form-element tm-form-element-2">
+                                            <button type="submit" class="btn btn-primary tm-btn-search">Check Availability</button>
+                                        </div>
+                                      </div>
+                                      
+                                </form>
+                            </div>                        
+                        </div>      
+                    </div>
+                </div>                  
+            </div>
+            </div>
+                    </div> 
+                </div> 
+            </div> 
+        </div>
+    </header> 
+      
+    <div class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="text-container about">
+                        <h4></h4>
+                        <p class="white"></p>
+                    </div>
+                </div> 
+                <div class="col-md-2">
+                    <div class="text-container">
+                        <h4>Help</h4>
+                        <ul class="list-unstyled li-space-lg white">
+                           <li>
+
+                           </li>
+                        </ul>
+                    </div>
+                </div> 
+                <div class="col-md-2">
+                    <div class="text-container">
+                        <h4>Social Media</h4>
+                        <ul class="list-unstyled li-space-lg">
+                            <li>
+                                
+                            </li>
+                        </ul>
+                    </div> 
+                </div> 
+                </div> 
+        </div> 
+    </div> 
+
+    
+    
+ 
+</div>
+        )
+    }
+ }
+
+ export default TripPlan;
