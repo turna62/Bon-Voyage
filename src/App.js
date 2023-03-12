931208240a6501922521e10677b01a29abe1578c
shaisad <95139737+shaisad@users.noreply.github.com>
2023-03-13 02:36:17 +0600

    image added for MyTrip

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import SignUp from "./user_creation/signup";
 import LogIn from "./user_creation/login";
 import HomePage from "./Home/home";
 import TripPlan from "./Home/PlanATrip";
-import Mytrip from "./MyTrip/MyTrip";
+import MyTrip from "./MyTrip/MyTrip";
 
 
 
@@ -25,7 +25,7 @@ function App() {
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/sign-in" element={<LogIn />} />
         <Route path="/planatrip" element={<TripPlan />} />
-        <Route path="/mytrip" element={<Mytrip />} />
+        <Route path="/mytrip" element={<MyTrip />} />
       </Routes>
     </Router>
   );
