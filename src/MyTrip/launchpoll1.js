fa5aa3a8382c1962f1e7d4a4213c8d5996b79cac
Lamia Alam Turna <95170568+turna62@users.noreply.github.com>
2023-04-29 15:19:21 +0600

    add poll ui

--- src/MyTrip/launchpoll1.js
+++ src/MyTrip/launchpoll1.js
@@ -130,7 +130,7 @@ function LaunchPoll1() {
         </div>
       </div>
       <div class="pphead">
-    <h3>Polls</h3>
+    <h3 class="head">Polls</h3>
 <p>Create another poll to help your group narrow down options or answer key questions.</p> 
 
 </div> 
@@ -139,41 +139,7 @@ function LaunchPoll1() {
 
     </div>
 
-    <div class="footer">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="text-container about">
-                        <h4></h4>
-                        <p class="white"></p>
-                    </div>
-                </div> 
-                <div class="col-md-2">
-                    <div class="text-container">
-                        <h4>Help</h4>
-                        <ul class="list-unstyled li-space-lg white">
-                           <li>
-
-                           </li>
-                        </ul>
-                    </div>
-                </div> 
-                <div class="col-md-2">
-                    <div class="text-container">
-                        <h4>Social Media</h4>
-                        <ul class="list-unstyled li-space-lg">
-                            <li>
-                                
-                            </li>
-                        </ul>
-                    </div> 
-                </div> 
-                </div> 
-        </div> 
-    </div> 
-
-    
-        
+            
             </div>
 
   );
