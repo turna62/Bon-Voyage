8efa6f8e463f77d39caf14c6fb14606937394eec
shaisad <95139737+shaisad@users.noreply.github.com>
2023-05-05 03:54:40 +0600

    unnecessary comments deleted

--- src/MyTrip/launchpoll1.js
+++ src/MyTrip/launchpoll1.js
@@ -7,64 +7,7 @@ import './mytrip.css';
 import '../Home/HomeCss/styles.css';
 
  class LaunchPoll1 extends React.Component {
-//   const [selectedOption, setSelectedOption] = useState(null);
-//   const [totalVotes, setTotalVotes] = useState(0);
-//   const [hasVoted, setHasVoted] = useState(false);
-//   const [optionsList, setOptionsList] = useState([
-//     { id: 'opt-1', text: 'Answer 1', votes: 0 },
-//     { id: 'opt-2', text: 'Answer 2', votes: 0 },
-//     { id: 'opt-3', text: 'Answer 3', votes: 0 },
-//     { id: 'opt-4', text: 'Answer 4', votes: 0 },
-//     { id: 'opt-5', text: 'Answer 5', votes: 0 },
-//   ]);
 
-//   useEffect(() => {
-//     const sum = optionsList.reduce((acc, cur) => acc + cur, 0);
-//     setTotalVotes(sum);
-//   }, []);
-
-//   const handleOptionClick = (option) => {
-//     if (!hasVoted) {
-//       setSelectedOption(option);
-//       setHasVoted(true);
-//       const updatedOptions = optionsList.map((opt) => {
-//         if (opt.id === option) {
-//           return { ...opt, votes: opt.votes + 1 };
-//         }
-//         return opt;
-//       });
-//       setOptionsList(updatedOptions);
-//     }
-//   };
-
-//   const handleSubmit = () => {
-//     console.log(`Selected option: ${selectedOption}`);
-//     console.log(`Total votes: ${totalVotes}`);
-//     console.log(`Has voted: ${hasVoted}`);
-//   };
-
-//   const pollOptions = optionsList.map((option) => {
-//     const isSelected = selectedOption === option.id;
-//     const classNames = ['option'];
-//     if (isSelected) {
-//       classNames.push('selected');
-//     }
-    // return (
-    //   <li key={option.id} onClick={() => handleOptionClick(option.id)}>
-    //     <label htmlFor={option.id} className={classNames.join(' ')}>
-    //       <div className="row">
-    //         <div className="column">
-    //           <span className="circle"></span>
-    //           <span className="text">{option.text}</span>
-    //         </div>
-    //         <span className="votes">{option.votes} votes</span>
-    //       </div>
-    //       <input type="radio" name="poll" id={option.id} />
-    //     </label>
-    //   </li>
-    // );
-  
-  //);
 
   constructor(props) {
     super(props);
