cc6c1552356417fbb81d50f84f48e4ec1180a25a
Lamia Alam Turna <95170568+turna62@users.noreply.github.com>
2023-05-14 15:59:07 +0600

    maps success

--- src/MyTrip/routes.js
+++ src/MyTrip/routes.js
@@ -1,207 +1,133 @@
-/// nice one
-
-import { useState, useMemo } from "react";
-import './mytrip.css';
-import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
-import usePlacesAutocomplete, {
-  getGeocode,
-  getLatLng,
-} from "use-places-autocomplete";
+import React, { useState, useRef } from "react";
 import {
-  Combobox,
-  ComboboxInput,
-  ComboboxPopover,
-  ComboboxList,
-  ComboboxOption,
-} from "@reach/combobox";
-import "@reach/combobox/styles.css";
+  GoogleMap,
+  useJsApiLoader,
+  Marker,
+  Autocomplete,
+  DirectionsService,
+  DirectionsRenderer,
+} from "@react-google-maps/api";
 
-export default function Places() {
-  const { isLoaded } = useLoadScript({
+const center = { lat: 48.8584, lng: 2.2945 };
+
+function Places() {
+  const [map, setMap] = useState(null);
+  const [directionsResponse, setDirectionsResponse] = useState(null);
+  const [distance, setDistance] = useState("");
+  const [duration, setDuration] = useState("");
+
+  const originRef = useRef();
+  const destinationRef = useRef();
+
+  const { isLoaded } = useJsApiLoader({
     googleMapsApiKey: "AIzaSyAz2_MkHBuMmmgsKwwVnp1tF-qOVm0B9Oo",
     libraries: ["places"],
   });
 
-  if (!isLoaded) return <div>Loading...</div>;
-  return (
-    <div class="deetailplan">
+  function calculateRoute() {
+    if (
+      originRef.current.value === "" ||
+      destinationRef.current.value === ""
+    ) {
+      return;
+    }
+    const directionsService = new DirectionsService();
+    directionsService.route(
+      {
+        origin: originRef.current.value,
+        destination: destinationRef.current.value,
+        travelMode: "DRIVING",
+      },
+      (results, status) => {
+        if (status === "OK") {
+          setDirectionsResponse(results);
+          setDistance(results.routes[0].legs[0].distance.text);
+          setDuration(results.routes[0].legs[0].duration.text);
+        } else {
+          console.error(`error fetching directions ${status}`);
+        }
+      }
+    );
+  }
 
-         <nav class="navbar navbar-expand-md navbar-dark navbar-custom fixed-top">
-                <h3 class="logo">Bon VOYAGE!</h3>
-                <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item">
-                            <a class="nav-link page-scroll" href="http://localhost:3000">HOME <span class="sr-only">(current)</span></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link page-scroll" href="#intro">LOG OUT</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link page-scroll" href="http://localhost:3000/myprofile">MY PROFILE</a>
-                        </li>
-                       
-                        
-                  </ul>
-
-              </div>
-         </nav>
-
-         <header id="header" class="headerr">
-                <div class="header-content">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-lg-12">
-                                <div class="text-container">
-                                <h1>Let's Plan!</h1>
-                                    {/* <p class="p-heading p-large">The journey of a thousand miles begins with a single step.</p> */}
-
-
-                                </div>
-                            </div> 
-                        </div> 
-                    </div>
-                </div> 
-            </header> 
-
-
-     <div>
-
-        <h4 class="tripname">Trip Name</h4><hr></hr>
-        <a class="btnaddmembers" href="http://localhost:3000/addmembers">+ Add Members</a>
-        <ul class="ul">
-        <li class="li"><a href="http://localhost:3000/overview">Overview</a></li>
-        <li class="li"><a href="http://localhost:3000/polls">Polls</a></li>
-        <li class="li"><a href="http://localhost:3000/date">Date</a></li>
-        <li class="li"><a href="http://localhost:3000/destination">Destination</a></li>
-        <li class="ovwli"><a href="http://localhost:3000/route">Route</a></li>
-        <li class="li"><a href="http://localhost:3000/itinerary">Itinerary</a></li>
-     </ul>
-     </div> 
-
-
-  <Map />
-
-  <div class="footer">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="text-container about">
-                        <h4>Few Words About Bon Vogage!</h4>
-                        <p class="white">We are passionate about helping you to arrange your trip as best as we can.</p></div>
-                </div> 
-                <div class="col-md-2">
-                    <div class="text-container">
-                        <h4>Links</h4>
-                        <ul class="list-unstyled li-space-lg white">
-                            <li>
-                                <a class="white" href="#your-link">startupguide.com</a>
-                            </li>
-                            <li>
-                                <a class="white" href="terms-conditions.html">Terms & Conditions</a>
-                            </li>
-                            <li>
-                                <a class="white" href="privacy-policy.html">Privacy Policy</a>
-                            </li>
-                        </ul>
-                    </div> 
-                </div> 
-                <div class="col-md-2">
-                    <div class="text-container">
-                        <h4>Tools</h4>
-                        <ul class="list-unstyled li-space-lg">
-                            <li>
-                                <a class="white" href="#your-link">businessgrowth.com</a>
-                            </li>
-                            <li>
-                               <a class="white" href="#your-link">influencers.com</a>
-                            </li>
-                            <li class="media">
-                                <a class="white" href="#your-link">optimizer.net</a>
-                            </li>
-                        </ul>
-                    </div> 
-                </div> 
-                <div class="col-md-2">
-                    <div class="text-container">
-                        <h4>Partners</h4>
-                        <ul class="list-unstyled li-space-lg">
-                            <li>
-                                <a class="white" href="#your-link">unicorns.com</a>
-                            </li>
-                            <li>
-                                <a class="white" href="#your-link">staffmanager.com</a>
-                            </li>
-                            <li>
-                                <a class="white" href="#your-link">association.gov</a>
-                            </li>
-                        </ul>
-                    </div> 
-                </div> 
-            </div>
-        </div> 
-    </div> 
-
-  </div>
-
-  );
-}
-
-function Map() {
-  const center = useMemo(() => ({ lat: 43.45, lng: -80.49 }), []);
-  const [selected, setSelected] = useState(null);
+  function clearRoute() {
+    setDirectionsResponse(null);
+    setDistance("");
+    setDuration("");
+    originRef.current.value = "";
+    destinationRef.current.value = "";
+  }
 
   return (
-    <>
-      <div className="places-container">
-        <PlacesAutocomplete setSelected={setSelected} />
-      </div>
-
-      <GoogleMap
-        zoom={10}
-        center={center}
-        mapContainerClassName="map-container"
+    <div style={{ position: "relative", height: "100vh", width: "100vw" }}>
+      {isLoaded ? (
+        <GoogleMap
+          center={center}
+          zoom={15}
+          options={{
+            zoomControl: false,
+            streetViewControl: false,
+            mapTypeControl: false,
+            fullscreenControl: false,
+          }}
+          onLoad={(map) => setMap(map)}
+        >
+          <Marker position={center} />
+          {directionsResponse && (
+            <DirectionsRenderer directions={directionsResponse} />
+          )}
+        </GoogleMap>
+      ) : (
+        <div>Loading...</div>
+      )}
+      <div
+        style={{
+          position: "absolute",
+          top: "4rem",
+          right: "2rem",
+          zIndex: "1",
+          backgroundColor: "white",
+          padding: "1rem",
+          borderRadius: "0.5rem",
+          boxShadow: "0 0 0.5rem rgba(0, 0, 0, 0.25)",
+        }}
       >
-        {selected && <Marker position={selected} />}
-      </GoogleMap>
-    </>
-  );
+        <div style={{ display: "flex", marginBottom: "0.5rem" }}>
+          <Autocomplete onLoad={(autocomplete) => autocomplete}>
+            <input
+              type="text"
+              placeholder="Origin"
+              ref={originRef}
+              style={{ flexGrow: 1, marginRight: "0.5rem" }}
+            />
+          </Autocomplete>
+          <Autocomplete onLoad={(autocomplete) => autocomplete}>
+            <input
+              type="text"
+              placeholder="Destination"
+              ref={destinationRef}
+              style={{ flexGrow: 1, marginRight: "0.5rem" }}
+            />
+          </Autocomplete>
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <button onClick={calculateRoute}>Calculate Route</button>
+            <button onClick={clearRoute}>Clear Route</button>
+</div>
+</div>
+{directionsResponse && (
+<div style={{ display: "flex", alignItems: "center" }}>
+<div style={{ marginRight: "0.5rem" }}>Distance: {distance}</div>
+<div>Duration: {duration}</div>
+</div>
+)}
+</div>
+</div>
+);
 }
 
-const PlacesAutocomplete = ({ setSelected }) => {
-  const {
-    ready,
-    value,
-    setValue,
-    suggestions: { status, data },
-    clearSuggestions,
-  } = usePlacesAutocomplete();
+export default Places;
 
-  const handleSelect = async (address) => {
-    setValue(address, false);
-    clearSuggestions();
 
-    const results = await getGeocode({ address });
-    const { lat, lng } = await getLatLng(results[0]);
-    setSelected({ lat, lng });
-  };
 
-  return (
-    <Combobox onSelect={handleSelect}>
-      <ComboboxInput
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-        disabled={!ready}
-        className="combobox-input"
-        placeholder="Search an address"
-      />
-      <ComboboxPopover>
-        <ComboboxList>
-          {status === "OK" &&
-            data.map(({ place_id, description }) => (
-              <ComboboxOption key={place_id} value={description} />
-            ))}
-        </ComboboxList>
-      </ComboboxPopover>
-    </Combobox>
-  );
-};
+
+
