1fbb18c92e445346cae301a0d1372b23256e6b30
shaisad <95139737+shaisad@users.noreply.github.com>
2023-05-19 01:50:18 +0600

    link fixes + icons set in all segments

--- src/MyTrip/destination.js
+++ src/MyTrip/destination.js
@@ -15,14 +15,140 @@ import usePlacesAutocomplete, {
   } from "@reach/combobox";
   import "@reach/combobox/styles.css";  
 
-  export default function Destination() {
-    const { isLoaded } = useLoadScript({
-        googleMapsApiKey: "AIzaSyAz2_MkHBuMmmgsKwwVnp1tF-qOVm0B9Oo",
-        libraries: ["places"],
-      });
-    
-      if (!isLoaded) return <div>Loading...</div>;   
+  export default class Destination extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+          userId: null,
+          tripId: null,
+          tripData:"",
+          userData:"",
+          notifsData:[],
+          
+        };
+        this.updateAllIsRead = this.updateAllIsRead.bind(this);
+      }
+
+      componentDidMount() {
+        const params = new URLSearchParams(window.location.search);
+        const userId = params.get('userId');
+        const tripId = params.get('tripId');
+        
+        console.log(userId); 
+        console.log(tripId);
+        this.setState({ userId: userId });
+        this.setState({ tripId: tripId });
+        
+
+    
+  
+    fetch("http://localhost:5000/notifications",{
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({
+          tripId: tripId,
+          userId: userId
+        }),
+      })
+      .then((res) => res.json()) // convert data into JSON
+      .then((data) => {
+        console.log(data, "notifsData");
+        this.setState({notifsData: data});
+      });
+      
+
+        fetch("http://localhost:5000/tripData",{
+            method: "POST",
+            crossDomain: true,
+            headers:{
+                "Content-Type":"application/json",
+                Accept: "application/json",
+                "Access-Control-Allow-Origin": "*",
+            },
+            body: JSON.stringify({
+                
+                tripId: tripId,
+            
+            }),
+        })
+        .then((res) => res.json()) // convert data into JSON
+        .then((data) => {
+            console.log(data, "tripData");
+            this.setState({tripData: data.data});
+            if(data.data == 'Token Expired!'){
+                alert("Token expired! Kindly login again."); 
+                window.localStorage.clear();
+                window.location.href = "./sign-in";
+            }
+        });
+
+        fetch("http://localhost:5000/userData",{
+            method: "POST",
+            crossDomain: true,
+            headers:{
+                "Content-Type":"application/json",
+                Accept: "application/json",
+                "Access-Control-Allow-Origin": "*",
+            },
+            body: JSON.stringify({
+                token: window.localStorage.getItem("token"),
+                userId: userId,
+            
+            }),
+        })
+        .then((res) => res.json()) // convert data into JSON
+        .then((data) => {
+            console.log(data, "userData");
+            this.setState({userData: data.data});
+            if(data.data == 'Token Expired!'){
+                alert("Token expired! Kindly login again."); 
+                window.localStorage.clear();
+                window.location.href = "./sign-in";
+            }
+        });
+
+      }
+    
+
+      updateAllIsRead = () => {
+         const params = new URLSearchParams(window.location.search);
+        const userId = params.get('userId');
+        const tripId = params.get('tripId');
+        
+        console.log(userId); 
+        console.log(tripId);
+        this.setState({ userId: userId });
+        this.setState({ tripId: tripId });
+        const unreadNotifs = this.state.notifsData.filter((notif) => !notif.isRead);
+        const unreadNotifIds = unreadNotifs.map((notif) => notif._id);
+
+      
+        fetch('http://localhost:5000/notifications/read', {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+          },
+          body: JSON.stringify({ userId:userId, tripId:tripId }),
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            // Update the notifsData state to mark all notifications as read
+            const updatedNotifsData = this.state.notifsData.map((notif) => ({
+              ...notif,
+              isRead: true,
+            }));
+            this.setState({ notifsData: updatedNotifsData });
+          })
+          .catch((error) => console.error(error));
+      };
+
+    render(){
+    
     
 
         return(
@@ -33,15 +159,44 @@ import usePlacesAutocomplete, {
                 <div class="collapse navbar-collapse" id="navbarsExampleDefault">
                     <ul class="navbar-nav ml-auto">
                     <li class="nav-item">
-                            <a class="nav-link page-scroll" href="http://localhost:3000"><i class="fa fa-home"></i> HOME <span class="sr-only">(current)</span></a>
+                        
+
+                        <a class="nav-link page-scroll">
+  <button onClick={this.updateAllIsRead}>
+    <span class="notif-icon">
+      <i class="fas fa-bell"></i>
+      {this.state.notifsData.filter(notif => !notif.isRead).length > 0 && (
+        <span class="notif-count">
+          {this.state.notifsData.filter(notif => !notif.isRead).length}
+        </span>
+      )}
+    </span>
+  </button>
+</a> 
+<div class="notifications-container">
+  <ul>
+    {this.state.notifsData.map((notif, index) => (
+      <li key={index}>
+        <p>{notif.message}</p>
+        <p>{notif.createdAt}</p>
+      </li>
+    ))}
+  </ul>
+</div>
+ 
+  
+  
+</li>
+<li class="nav-item">
+ <a class="nav-link page-scroll" href="http://localhost:3000"><i class="fa fa-home"></i> HOME <span class="sr-only">(current)</span></a>
+                        </li>
+                        
+                        <li class="nav-item">
+                            <a class="nav-link page-scroll" href={`http://localhost:3000/myprofile?userId=${encodeURIComponent(this.state.userId)}`}> <i class='fas fa-user-circle'></i> MY PROFILE</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link page-scroll" href="#intro"><i class="fa fa-sign-out"></i> LOG OUT</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link page-scroll" href={`http://localhost:3000/myprofile`}> <i class='fas fa-user-circle'></i> MY PROFILE</a>
-                        </li>
-                       
                         
                   </ul>
 
@@ -54,7 +209,7 @@ import usePlacesAutocomplete, {
                         <div class="row">
                             <div class="col-lg-12">
                                 <div class="text-container">
-                                <h1></h1>
+                                <h1>Let's Plan, {this.state.userData.username}!</h1>
 
 
                                 </div>
@@ -67,16 +222,17 @@ import usePlacesAutocomplete, {
 
      <div>
 
-     <h4 class="tripname">trip name</h4><hr></hr>
-     <a class="btnaddmembers" href={`http://localhost:3000/addmembers`}>+ Add Members</a>
+     <h4 class="tripname">{this.state.tripData.tripName}</h4><hr></hr>
+     <a class="btnaddmembers" href={`http://localhost:3000/addmembers?userId=${encodeURIComponent(this.state.userId)}&tripId=${encodeURIComponent(this.state.tripId)}`}>+ Add Members</a>
         <ul class="ul">
-        <li class="li"><a href="http://localhost:3000/overview">Overview</a></li>
-        <li class="li"><a href="http://localhost:3000/polls">Polls</a></li>
-        <li class="li"><a href="http://localhost:3000/date">Date</a></li>
-        <li class="ovwli"><a href="http://localhost:3000/destination">Destination</a></li>
-        <li class="li"><a href="http://localhost:3000/route">Route</a></li>
-        <li class="li"><a href="http://localhost:3000/itinerary">Itinerary</a></li>
-     </ul>     </div>     
+        <li class="li"><a href={`http://localhost:3000/overview?userId=${encodeURIComponent(this.state.userId)}&tripId=${encodeURIComponent(this.state.tripId)}`}>Overview</a></li>
+        <li class="li"><a href={`http://localhost:3000/polls?userId=${encodeURIComponent(this.state.userId)}&tripId=${encodeURIComponent(this.state.tripId)}`}>Polls</a></li>
+        <li class="li"><a href={`http://localhost:3000/date?userId=${encodeURIComponent(this.state.userId)}&tripId=${encodeURIComponent(this.state.tripId)}`}>Date</a></li>
+        <li class="ovwli"><a href={`http://localhost:3000/destination?userId=${encodeURIComponent(this.state.userId)}&tripId=${encodeURIComponent(this.state.tripId)}`}>Destination</a></li>
+        <li class="li"><a href={`http://localhost:3000/route?userId=${encodeURIComponent(this.state.userId)}&tripId=${encodeURIComponent(this.state.tripId)}`}>Route</a></li>
+        <li class="li"><a href={`http://localhost:3000/itinerary?userId=${encodeURIComponent(this.state.userId)}&tripId=${encodeURIComponent(this.state.tripId)}`}>Itinerary</a></li>
+     </ul>     
+     </div>     
      
      <div class="desbody">
      <div class="phead">  
@@ -159,10 +315,19 @@ import usePlacesAutocomplete, {
         );
 
     }
+}
 
 
     function Map() {
         const [setSelected] = useState(null);
+
+        const { isLoaded } = useLoadScript({
+            googleMapsApiKey: "AIzaSyAz2_MkHBuMmmgsKwwVnp1tF-qOVm0B9Oo",
+            libraries: ["places"],
+          });
+        
+          if (!isLoaded) return <div>Loading...</div>;   
+    
       
         return (
           <>
--- src/MyTrip/itinerary.js
+++ src/MyTrip/itinerary.js
@@ -10,8 +10,11 @@ class Itinerary extends React.Component{
           tripId: null,
           tripData:"",
           userData:"",
-          destination: ""
+          destination: "",
+          notifsData:[],
         };
+        this.updateAllIsRead = this.updateAllIsRead.bind(this);
+      
         //this.handleSubmit = this.handleSubmit.bind(this); // to read properties of state
       }
     
@@ -25,7 +28,23 @@ class Itinerary extends React.Component{
         this.setState({ userId: userId });
         this.setState({ tripId: tripId });
         
-        
+        fetch("http://localhost:5000/notifications",{
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({
+          tripId: tripId,
+          userId: userId
+        }),
+      })
+      .then((res) => res.json()) // convert data into JSON
+      .then((data) => {
+        console.log(data, "notifsData");
+        this.setState({notifsData: data});
+      });
+      
 
 
         fetch("http://localhost:5000/tripData",{
@@ -80,46 +99,40 @@ class Itinerary extends React.Component{
 
       }
 
-    //   handleSubmit(e){
-    //     e.preventDefault();
-    //     const { destination, userId, tripId } = this.state;
-        
-    //     console.log(destination, userId, tripId);
-    //     fetch("http://localhost:5000/itinerary", {
-    //       method: "POST",
-    //       crossDomain: true,
-    //       headers: {
-    //         "Content-Type": "application/json",
-    //         Accept: "application/json",
-    //         "Access-Control-Allow-Origin": "*",
-    //         authorization: localStorage.getItem("userId") ,
-    //       //  authorization: localStorage.getItem("email") ,
-    //       },
-    //       body: JSON.stringify({
-    //         destination,
-    //         userId,
-    //         tripId
-            
-    //       }),
-    //     })
-    //       .then((res) => res.json())
-    //       .then((data) => {
-    //         console.log(data, "itinerarySubmit");
-    //         if (data.status === "OK!") {
-                
-    //             alert('submitted Successfully!');
-    //             window.localStorage.setItem('itineraryId', data.itineraryId);
-    //             window.location.href = `http://localhost:3000/myitinerary?userId=${encodeURIComponent(this.state.userId)}&tripId=${encodeURIComponent(this.state.tripId)}&itineraryId=${data.itineraryId}`;
+      updateAllIsRead = () => {
+        const params = new URLSearchParams(window.location.search);
+       const userId = params.get('userId');
+       const tripId = params.get('tripId');
+       
+       console.log(userId); 
+       console.log(tripId);
+       this.setState({ userId: userId });
+       this.setState({ tripId: tripId });
+       const unreadNotifs = this.state.notifsData.filter((notif) => !notif.isRead);
+       const unreadNotifIds = unreadNotifs.map((notif) => notif._id);
 
-    //         } else {
-    //           alert(`went wrong: ${data.status}`);
-    //         }
-    //       })
-    //       .catch((error) => {
-    //         console.error(error);
-    //         alert("Error! Something went wrong while calling the API.");
-    //       });
-    //   }
+     
+       fetch('http://localhost:5000/notifications/read', {
+         method: 'PUT',
+         headers: {
+           'Content-Type': 'application/json',
+           Accept: 'application/json',
+         },
+         body: JSON.stringify({ userId:userId, tripId:tripId }),
+       })
+         .then((res) => res.json())
+         .then((data) => {
+           // Update the notifsData state to mark all notifications as read
+           const updatedNotifsData = this.state.notifsData.map((notif) => ({
+             ...notif,
+             isRead: true,
+           }));
+           this.setState({ notifsData: updatedNotifsData });
+         })
+         .catch((error) => console.error(error));
+     };
+     
+         
     
     render(){
 
@@ -131,13 +144,43 @@ class Itinerary extends React.Component{
                 <div class="collapse navbar-collapse" id="navbarsExampleDefault">
                     <ul class="navbar-nav ml-auto">
                     <li class="nav-item">
+                        
+
+                        <a class="nav-link page-scroll">
+  <button onClick={this.updateAllIsRead}>
+    <span class="notif-icon">
+      <i class="fas fa-bell"></i>
+      {this.state.notifsData.filter(notif => !notif.isRead).length > 0 && (
+        <span class="notif-count">
+          {this.state.notifsData.filter(notif => !notif.isRead).length}
+        </span>
+      )}
+    </span>
+  </button>
+</a> 
+<div class="notifications-container">
+  <ul>
+    {this.state.notifsData.map((notif, index) => (
+      <li key={index}>
+        <p>{notif.message}</p>
+        <p>{notif.createdAt}</p>
+      </li>
+    ))}
+  </ul>
+</div>
+ 
+  
+  
+</li>
+                    <li class="nav-item">
                             <a class="nav-link page-scroll" href="http://localhost:3000"><i class="fa fa-home"></i> HOME <span class="sr-only">(current)</span></a>
+                        </li>
+                      
+                        <li class="nav-item">
+                            <a class="nav-link page-scroll" href={`http://localhost:3000/myprofile?userId=${encodeURIComponent(this.state.userId)}`}> <i class='fas fa-user-circle'></i> MY PROFILE</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link page-scroll" href="#intro"><i class="fa fa-sign-out"></i> LOG OUT</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link page-scroll" href={`http://localhost:3000/myprofile?userId=${encodeURIComponent(this.state.userId)}`}> <i class='fas fa-user-circle'></i> MY PROFILE</a>
                         </li>
                        
                         
@@ -164,7 +207,7 @@ class Itinerary extends React.Component{
      <div>
 
         <h4 class="tripname">{this.state.tripData.tripName}</h4><hr></hr>
-        <a class="btnaddmembers" href="http://localhost:3000/addmembers">+ Add Members</a>
+        <a class="btnaddmembers" href={`http://localhost:3000/addmembers?userId=${encodeURIComponent(this.state.userId)}&tripId=${encodeURIComponent(this.state.tripId)}`}>+ Add Members</a>
 
         <ul class="ul">
         <li class="li"> <a href={`http://localhost:3000/overview?userId=${encodeURIComponent(this.state.userId)}&tripId=${encodeURIComponent(this.state.tripId)}`}>Overview</a></li>
